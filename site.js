ec9fac34cc6fd8edf49502e5875b8777088d7e5a
ardalan@mobilefringe.com @ CodeCloud <ardalan@mobilefringe.com @ CodeCloud>
2016-06-01 17:52:34 -0400

    Costa Mesa 1.0  /site.js

---
+++ site.js
@@ -0,0 +1,6 @@
+var site_json = {
+                    "name" : "Costa Mesa Courtyards",
+                    "default_image" : "http://assets.codecloudapp.com/sites/574f53a46e6f643cc5060000/image/jpeg/undefined/cmc_500x500_logo.jpg",
+                    "time_zone" : "T08:00:00Z",
+                    "instagram" : "196558634"
+                }
