11b22b4fed86e3f2f8cdbc57ee883d3316032c5a
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-06-13 13:34:13 -0400

    Costa Mesa 1.0  /site.js

--- site.js
+++ site.js
@@ -2,5 +2,5 @@ var site_json = {
                     "name" : "Costa Mesa Courtyards",
                     "default_image" : "http://assets.codecloudapp.com/sites/574f53a46e6f643cc5060000/image/jpeg/undefined/cmc_500x500_logo.jpg",
                     "time_zone" : "T08:00:00Z",
-                    "instagram" : "196558634"
+                    "social_feed": "http://costamesa.mallmaverick.com/api/v2/costamesa/social.json"
                 }
