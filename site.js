fbb9c1e5c0d922676e3171aafe652bf50cfb8bd7
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2016-10-04 12:20:21 -0400

    Costa Mesa 1.1  /site.js

---
+++ site.js
@@ -0,0 +1,6 @@
+var site_json = {
+                    "name" : "Costa Mesa Courtyards",
+                    "default_image" : "//codecloud.cdn.speedyrails.net/sites/57f3d3736e6f64604a010000/image/jpeg/undefined/cmc_500x500_logo.jpg",
+                    "time_zone" : "T08:00:00Z",
+                    "social_feed": "//costamesa.mallmaverick.com/api/v2/costamesa/social.json"
+                }
